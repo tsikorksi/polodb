4e2577caa62a27be721b6514c6126238d21676b1
tsikorksi <sikorski.t@etoncollege.org.uk>
2017-10-23 22:09:50 -0400

    finally fixed that data bullshit now we can do some shit

--- web.py
+++ web.py
@@ -1,32 +1,37 @@
 from flask import Flask, render_template, request
-from forms import InputForm
 app = Flask(__name__)
 app.secret_key = "development-key"
-ponies = open("data"+"/ponies.txt", "a")
-venues = open("data"+"/venues.txt", "a")
-players = open("data"+"/players.txt", "a")
 
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    error = None
-    # form = InputForm()
+    """
+    Basic web service for data entry
+    :return:
+    """
     if request.method == "POST":
-        pony_name = request.form["pony"]
-        ponies.write(pony_name + '\n')
+        ponies = open("data" + "/ponies.txt", "w")
+        venues = open("data" + "/venues.txt", "w")
+        players = open("data" + "/players.txt", "w")
         venue = request.form["venue"]
         venues.write(venue + '\n')
+        pony_name = request.form["pony"]
+        ponies.write(pony_name + '\n')
         player = request.form["player"]
         players.write(player + '\n')
-        players.write("test")
         return render_template('entry_menu.html'), 200
     elif request.method == "GET":
-        return render_template('entry_menu.html', error=error), 404
-    return render_template('entry_menu.html')
+        return render_template('entry_menu.html', error=True), 400
+    return render_template('entry_menu.html', error=False)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """
+    404
+    :param error:
+    :return:
+    """
     return render_template('page_not_found.html'), 404
 
 
