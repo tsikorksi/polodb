1157f0ca020bef3a8cb47009f3ed21d5f0c25af0
tsikorksi <sikorski.t@etoncollege.org.uk>
2017-11-13 22:13:00 +0000

    maths, bubble sort and backend player stats done updated test data

--- forms.py
+++ forms.py
@@ -2,6 +2,20 @@
 This file will take and compare the data in coredb.txt
 """
 import encrypt
+import math
+
+
+def bubble_sort(arr):
+    """
+    bubble sort
+    :param arr: list
+    :return arr: list
+    """
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
 
 def split(input_str):
@@ -28,18 +42,60 @@ def data_decode():
     output_data = []
     with open("data" + "/coredb.txt") as f:
         data = f.read().splitlines()
+        # print(data)
         for i in range(0, len(data)):
-            # mid_data = []
-            # mid_data.append(split(data[i]))
             output_data.append(split(encrypt.shift_decode(data[i], 5)))
-        print(output_data)
+    # print(output_data)
     return output_data
 
 
-def main_stats(name):
+def maths(values, mean):
+    """
+    :param values:
+    :param mean:
+    :return:
+    """
+    std_values = []
+    # median calculation
+    med = (len(values) - 1) / 2
+    if med.is_integer() is False:
+        median = (values[int(med + 0.5)] + values[int(med - 0.5)]) / 2
+    else:
+        median = values[int(med)]
+    # highest value calculation
+    max_val = values[len(values) - 1]
+    # lowest value calculation
+    min_val = values[0]
+    # standard deviation calculation
+    for i in range(0, len(values)):
+        std_values.append(values[i] ** 2)
+    half_way = (sum(std_values)) / (len(values))
+    std_dev = math.sqrt((half_way - (mean ** 2)))
+    print(mean, median, max_val, min_val, std_dev)
+    return median, max_val, min_val, std_dev
+
+
+def player_stats(player):
     """
     needs work, doesnt work yet
     :param: str
     :return:
     """
+    values = []
+    sum_val, count = 0, 0
     input_array = data_decode()
+    # mean calculation
+    for i in range(0, len(input_array)):
+        if input_array[i][0] == player:
+            count += 1
+            values.append(int(input_array[i][3]))
+            sum_val += int(input_array[i][3])
+    mean = sum_val/count
+    # sort
+    bubble_sort(values)
+    print(values)
+    median, max_val, min_val, std_dev = maths(values, mean)
+    return mean, median, max_val, min_val, std_dev
+
+
+player_stats('a')
